1ec046234d6987dc34e4ac6e5e07d4c560464fdd
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-08-22 17:54:17 +0100

    generated author & post model using app console generator

---
+++ src/Blog/ModelBundle/Entity/Timestampable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Blog\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Timestampable abstract class to define created behaviour
+ * @ORM\MappedSuperclass
+ */
+abstract class Timestampable
+{
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Author
+     */
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+}
