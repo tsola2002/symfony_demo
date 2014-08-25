23c22b2a2dff492b0cfea226bee3e5cdc593ae35
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-08-25 15:16:18 +0100

    generated controller

--- src/Blog/CoreBundle/Controller/PostController.php
+++ src/Blog/CoreBundle/Controller/PostController.php
@@ -6,7 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class PostController extends Controller
 {
+    /**
+     * Show the post index
+     *
+     * @return array
+     *
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
 
 }
