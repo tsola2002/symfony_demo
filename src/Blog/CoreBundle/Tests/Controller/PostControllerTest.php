fd230b5f0b3e088c582c1da35ce3f8b297dcc952
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-08-26 16:22:25 +0100

    added sample test file to be used in symfony framework

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -4,13 +4,24 @@ namespace Blog\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+
+/**
+ * Class PostControllerTest
+ * @package Blog\CoreBundle\Tests\Controller
+ */
 class PostControllerTest extends WebTestCase
 {
+
+    /**
+     *Test Post Index
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
     }
 
 }
