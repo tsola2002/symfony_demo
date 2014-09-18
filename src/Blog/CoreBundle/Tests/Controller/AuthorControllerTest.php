bb968d731476527b85d1160e3158307779c10785
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-09-18 11:53:33 +0100

    writing test to check author post list

--- src/Blog/CoreBundle/Tests/Controller/AuthorControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/AuthorControllerTest.php
@@ -17,7 +17,14 @@ class AuthorControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/show');
+        $author  = $client->getContainer()->get('doctrine')->getManager()->getRepository('ModelBundle:Author')->findFirst();
+        $authorPostsCount = $author->getPosts()->count();
+
+        $crawler = $client->request('GET', '/author/'.$author->getSlug());
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+
+        $this->assertCount($authorPostsCount, $crawler->filter('h2'), 'There should be' .$authorPostsCount. ' posts');
     }
 
 }
