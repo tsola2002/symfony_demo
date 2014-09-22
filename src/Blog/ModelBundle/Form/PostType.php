94f5b881f1421362d994dabda7bfe4b65f58b591
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-09-22 15:23:25 +0100

    genarating the crud for post

---
+++ src/Blog/ModelBundle/Form/PostType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Blog\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('body')
+            ->add('author')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\ModelBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'blog_modelbundle_post';
+    }
+}
