0d4ca740cba7b51b1f1c6662335402bf9961afb5
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-09-22 11:58:16 +0100

    generating crud functions through doctrine

---
+++ src/Blog/ModelBundle/Form/AuthorType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Blog\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AuthorType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\ModelBundle\Entity\Author'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'blog_modelbundle_author';
+    }
+}
